b689901f6739421acde4d996bf37f7b3d294efc6
apsol <apscarlos@hotmail.com>
2015-05-17 22:35:47 -0300

    Capitulo 2 - ejemplo 1
    Pasando eventos directamente. uso de metodo ON  y TRIGGER.

--- capitulo2/js/capitulo2.js
+++ capitulo2/js/capitulo2.js
@@ -4,7 +4,20 @@ $(document).ready(function(){
 
 
 function ejemplo2_1(){
-   
+	var objeto = {};
+	// extiende a objeto la capacidad de trabajar con eventos
+	_.extend(objeto, Backbone.Events);
+	//  on nos perite recibir eventos enviados por si mismo
+	objeto.on("alert", function(msg, el){
+		alert(msg);
+		el.append("<div class='tested'>Elemento Pasado</div>");
+	});
+	//usaremos jQuery para usar al usuario para interactuar.
+	$("#ejemplo1").click(function(event) {
+		//ahora necesitamos poder lanzar los eventos usamos trigger
+		objeto.trigger("alert","Pasando  parametros de eventos", $(this));
+	});
+	
 }
 
 
